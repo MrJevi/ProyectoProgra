9f819412ea10b0c8d6e9bc5bf5f54d9eed77d832
MrJevi <Jehnram@gmail.com>
2026-05-08 10:15:05 -0600

    Modelos libro, Usuario y Prestamos

---
+++ BibliotecaAPI_PRUEBA/DataStructure/Models/Prestamo.cs
@@ -0,0 +1,18 @@
+﻿namespace BibliotecaAPI.DataStructures.Models
+{
+    public class Prestamo
+    {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public Libro Libro { get; set; }
+        public DateTime FechaPrestamo { get; set; }
+
+        public Prestamo(int id, Usuario usuario, Libro libro)
+        {
+            Id = id;
+            Usuario = usuario;
+            Libro = libro;
+            FechaPrestamo = DateTime.Now;
+        }
+    }
+}
