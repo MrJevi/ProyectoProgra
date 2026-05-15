a36ef363fcda0dd8449f7ced19b228a45a375780
MrJevi <Jehnram@gmail.com>
2026-05-15 08:24:05 -0600

    update: sincronizacion total de modelos y servicios niveles 4-6

--- BibliotecaAPI_PRUEBA/DataStructure/Services/GestorBiblioteca.cs
+++ BibliotecaAPI_PRUEBA/DataStructure/Services/GestorBiblioteca.cs
@@ -1,33 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using BibliotecaAPI.DataStructures.Models;
 
 namespace BibliotecaAPI.DataStructures.Services
 {
     public class GestorBiblioteca
     {
-        // Estructuras principales
+       
         public List<Libro> ListaLibros { get; set; }
         public Dictionary<int, Libro> DiccionarioLibros { get; set; }
+
+       //Cola de préstamos
+        public Queue<Prestamo> ColaPrestamos { get; set; }
+
+        //Pila de historial
+        public Stack<string> HistorialAcciones { get; set; }
 
         public GestorBiblioteca()
         {
             ListaLibros = new List<Libro>();
             DiccionarioLibros = new Dictionary<int, Libro>();
+            
+            ColaPrestamos = new Queue<Prestamo>();
+            HistorialAcciones = new Stack<string>();
         }
 
         public void RegistrarNuevoLibro(Libro libro)
         {
             ListaLibros.Add(libro);
             DiccionarioLibros.Add(libro.Id, libro);
+           
+            HistorialAcciones.Push($"Libro registrado: {libro.Titulo}");
         }
 
-        public Libro BuscarPorId(int id)
+        
+        public void SolicitarPrestamo(Prestamo prestamo)
         {
-            if (DiccionarioLibros.ContainsKey(id))
-            {
-                return DiccionarioLibros[id];
-            }
-            return null;
+            ColaPrestamos.Enqueue(prestamo);
+            HistorialAcciones.Push($"Usuario {prestamo.Usuario.Nombre} entró en la fila de espera.");
+        }
+
+        
+        public string ObtenerUltimaActividad()
+        {
+            return HistorialAcciones.Count > 0 ? HistorialAcciones.Peek() : "No hay historial.";
         }
     }
 }
